a29cfafa02003dd6c0b59b3fb79a05883f139767
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-09-06 16:55:40 +0900

    [level 0] Title: x 사이의 개수, Time: 33.41 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Solution {
     public int[] solution(String myString) {
         String[] cut = myString.split("x");
-        String cutString = myString.replace("a","").replace("b","").replace("c","").replace("d","").replace("e","").replace("f","").replace("g","").replace("h","").replace("i","").replace("j","").replace("k","").replace("l","").replace("m","").replace("n","").replace("o","").replace("p","").replace("q","").replace("r","").replace("s","").replace("t","").replace("u","").replace("v","").replace("w","").replace("y","").replace("z","");
+        String cutString = myString.replaceAll("[^x]","");
         int[] answer = new int[cutString.length()+1];        
         
         for( int i= 0; i< cut.length; i++){
