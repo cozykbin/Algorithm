ca6a87febea3654157c3cc8f17da0c71a23d9dbc
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-16 17:47:55 +0900

    [Bronze V] Title: 검증수, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int I = Integer.parseInt(st.nextToken());
+		int A = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		int answer = H * H + I * I + A * A + R * R + C * C;
+		System.out.println(answer % 10);
+
+	}
+
+}
