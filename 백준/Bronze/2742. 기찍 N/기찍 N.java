5aef4aef8d1fabb7554ec80f399f59770683f304
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-16 17:28:07 +0900

    [Bronze V] Title: 기찍 N, Time: 136 ms, Memory: 17208 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(in.readLine());
+		
+		while(N!=0) {
+			sb.append(N--).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
