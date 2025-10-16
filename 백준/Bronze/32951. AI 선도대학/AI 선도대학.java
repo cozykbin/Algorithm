b1c9ecbf387b1529c1131289ccdc52bc3b5a46c2
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-16 17:36:53 +0900

    [Bronze V] Title: AI 선도대학, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(in.readLine());
+		System.out.println(N - 2024);
+
+	}
+
+}
