377373b641fc43af36a36d8a8f37f342c6c46ce4
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-17 17:47:08 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(in.readLine());
+		String star = "*";
+
+		for (int i = 0; i < N; i++) {
+			sb.append(star.repeat(i + 1)).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
