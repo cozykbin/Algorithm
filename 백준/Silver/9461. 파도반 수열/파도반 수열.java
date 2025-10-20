311ff4ada6c31ab918cd0bae2131b98b64912034
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-20 13:20:32 +0900

    [Silver III] Title: 파도반 수열, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(in.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+
+			N = Integer.parseInt(in.readLine());
+			long[] nums = new long[101];
+
+			nums[1] = 1;
+			nums[2] = 1;
+			nums[3] = 1;
+			nums[4] = 2;
+			nums[5] = 2;
+			
+			if (N > 5) {
+				for (int i = 6; i <= N; i++) {
+					nums[i] = nums[i - 1] + nums[i - 5];
+				}
+			}
+			sb.append(nums[N]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
