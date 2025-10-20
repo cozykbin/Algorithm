15c75fd26fa245c461486898a0d41d3e5e0c8e5f
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-21 00:06:52 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 60 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N;
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+			N = Integer.parseInt(in.readLine());
+			long[] nums = new long[117];
+
+			nums[1] = 1;
+			nums[2] = 1;
+			nums[3] = 1;
+			// f(n) = f(n-1) + f(n-3)
+			if (N > 3) {
+				for (int i = 4; i <= N; i++) {
+					nums[i] = nums[i - 1] + nums[i - 3];
+				}
+		}System.out.println(nums[N]);
+	}
+}
