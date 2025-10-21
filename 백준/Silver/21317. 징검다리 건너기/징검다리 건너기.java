7ff8f4048a0af43ccfdc83d4b9e3a4640223ef6f
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-22 01:06:29 +0900

    [Silver I] Title: 징검다리 건너기, Time: 64 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
+++ 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
@@ -12,73 +12,57 @@ public class Main {
 
 		N = Integer.parseInt(in.readLine());
 
-	    if (N == 1) {
-	         System.out.println(0);
-	         return;
-	      }
-		
-		sJump = new int[N - 1];
-		bJump = new int[N - 1];
+		if (N == 1) { // 돌이 1개면 점프 없이 종료
+			System.out.println(0);
+			return;
+		}
+
+		sJump = new int[N - 1]; // sJump[i] = (i+1 -> i+2) 돌 점프
+		bJump = new int[N - 1]; // bJump[i] = (i+1 -> i+3) 돌 점프
+
 		for (int i = 0; i < N - 1; i++) {
 			StringTokenizer st = new StringTokenizer(in.readLine());
 			sJump[i] = Integer.parseInt(st.nextToken());
 			bJump[i] = Integer.parseInt(st.nextToken());
 		}
 
-		K = Integer.parseInt(in.readLine());
+		K = Integer.parseInt(in.readLine()); // KJump (i -> i+3)
 
 		dp = new int[N];
-		dp[0] = 0;
+		dp[0] = 0; // 1번 돌
 		dp[1] = sJump[0];
 
-		
-		if (N == 2) {
-		         System.out.println(sJump[0]);
-	        return ;
-	      }else if (N == 3) {
-		         System.out.println(Math.min(sJump[0]+sJump[1],bJump[0]));
-	        return ;
-	      }
-		
-		// i, i+1 작은 점프 > 큰점 -> 큰점프 이득!
-		for (int i = 2; i < N; i++) {
-			dp[i] = Math.min(Math.min(dp[i - 1] + sJump[i - 1], dp[i - 2] + sJump[i - 2] + sJump[i - 1]),
-					dp[i - 2] + bJump[i - 2]);
+		if (N == 2) { // 돌이 2개면 sJump 1회 후 종료
+			System.out.println(dp[1]);
+			return;
 		}
 
-		int minEnergy = Integer.MAX_VALUE;
+		// 1. DP without K점프
+		for (int i = 2; i < N; i++) {
+			dp[i] = Math.min(dp[i - 1] + sJump[i - 1], dp[i - 2] + bJump[i - 2]);
+		}
 
-		for (int i = 3; i < N; i++) {
-			int[] dpHjump = new int[N + 1];
-			dpHjump[0] = dp[0];
-			dpHjump[1] = dp[1];
-			dpHjump[2] = dp[2];
-			// System.out.println("i: " + i);
+		int minEnergy = dp[N - 1]; // 일단 최소 E가 이건데..
+
+		// 2. DP with K점프
+		for (int i = 3; i < N; i++) { // 더 줄일 수 있는 세계관을 향해
+			int[] dpWithK = new int[N];
+
+			dpWithK[0] = dp[0];
+			dpWithK[1] = dp[1];
+			dpWithK[2] = dp[2];
+
+			// 4번돌 착지부터, K점프를 사용을 고려한 경우를 포함하여 계산하고 비교
 			for (int j = 3; j < N; j++) {
+				dpWithK[j] = Math.min(dpWithK[j - 1] + sJump[j - 1], dpWithK[j - 2] + bJump[j - 2]);
 
 				if (j == i) {
-					dpHjump[j] = Math.min(dp[j - 3] + K,
-							Math.min(
-									Math.min(dpHjump[j - 1] + sJump[j - 1],
-											dpHjump[j - 2] + sJump[j - 2] + sJump[j - 1]),
-									dpHjump[j - 2] + bJump[j - 2]));
-//					System.out.println("dp:" + dp[j - 3] + "K: " + K);
-
-				} else {
-					dpHjump[j] = Math.min(
-							Math.min(dpHjump[j - 1] + sJump[j - 1], dpHjump[j - 2] + sJump[j - 2] + sJump[j - 1]),
-							dpHjump[j - 2] + bJump[j - 2]);
+					dpWithK[j] = Math.min(dpWithK[j], dp[j - 3] + K);
 				}
-
 			}
-//			System.out.println("dpHjump[N - 1]: " + dpHjump[N - 1]);
-			minEnergy = Math.min(minEnergy, dpHjump[N - 1]);
-//			System.out.println("minEnergy" + minEnergy);
-//			System.out.println();
 
+			minEnergy = Math.min(minEnergy, dpWithK[N - 1]);
 		}
-
 		System.out.println(minEnergy);
-
 	}
 }
