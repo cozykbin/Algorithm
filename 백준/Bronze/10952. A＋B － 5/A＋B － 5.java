9b7b728af0e63fe5fd69fd67d4b9c412d734c68a
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-22 15:23:46 +0900

    [Bronze V] Title: A+B - 5, Time: 72 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		while ((input = in.readLine()) != null) {
+			
+			String[] inputs = input.split(" ");
+
+			int A = Integer.parseInt(inputs[0]);
+			int B = Integer.parseInt(inputs[1]);
+
+			if(A==0) {
+				return;
+			}
+			System.out.println(A + B);
+		}
+	}
+
+}
