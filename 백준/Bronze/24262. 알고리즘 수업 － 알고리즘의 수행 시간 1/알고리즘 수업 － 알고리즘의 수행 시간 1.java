0997ad1493e87224ac54724b8ce623e851ef87c0
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-28 17:36:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		StringBuilder sb = new StringBuilder();
+			sb.append(1).append("\n").append(0);
+
+			System.out.println(sb);
+
+
+	}
+
+}
