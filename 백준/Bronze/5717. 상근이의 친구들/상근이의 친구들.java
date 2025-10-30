622e5205863f23f5d99e6bec36246ce5ffbaf5e1
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-10-30 15:26:42 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 64 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		if(a==0 && b==0) {
+			return;
+		}
+
+		System.out.println(a+b);
+		}
+
+}
+}
