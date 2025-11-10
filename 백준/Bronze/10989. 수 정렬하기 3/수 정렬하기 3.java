135a33dc2881cc44b8bc37c9fe9fe9db39bdf847
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-11-10 16:50:41 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1648 ms, Memory: 481192 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -5,7 +5,8 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
+		StringBuilder sb = new StringBuilder();
+		
 		int N = Integer.parseInt(in.readLine());
 
 		int[] nums = new int[10001];
@@ -15,14 +16,12 @@ public class Main {
 			nums[a]++;
 		}
 		for (int i = 1; i < 10001; i++) {
-			if (nums[i] > 1) {
-				String idx = i+"\n";
-				System.out.print(idx.repeat(nums[i]-1));
-				System.out.println(i+"");
-			}else if(nums[i] ==1) {
-				String idx = i+ "";
-				System.out.println(idx);
+			while (nums[i] != 0) {
+				sb.append(i).append("\n");
+				nums[i]--;
+
 			}
 		}
+		System.out.println(sb);
 	}
 }
