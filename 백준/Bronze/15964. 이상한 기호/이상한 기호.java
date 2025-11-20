09f2793d631faf475e7576d7ffc6366ab2d50696
편안한윾이 <70093181+cozykbin@users.noreply.github.com>
2025-11-21 01:01:52 +0900

    [Bronze V] Title: 이상한 기호, Time: 64 ms, Memory: 11532 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+
+		System.out.println((A + B) * (A - B));
+	}
+}
