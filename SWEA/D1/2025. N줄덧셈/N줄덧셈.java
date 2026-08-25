b1dfe4ad96d07a96a6a8f52569a5ce7dd9b8f11d
cozykbin <70093181+cozykbin@users.noreply.github.com>
2026-08-25 13:30:21 +0900

    [D1] 2025 N줄덧셈

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int answer = n * (n + 1) / 2;
+
+		System.out.println(answer);
+	}
+}
---
+++ SWEA/D1/2025. N줄덧셈/README.md
@@ -0,0 +1,17 @@
+# [D1] N줄덧셈 - 2025
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QFZtaAscDFAUq)
+
+### 성능 요약
+
+메모리: 25,088 KB, 시간: 76 ms, 코드길이: 331 Bytes
+
+### 제출 결과
+
+Pass (1회차)
+
+### 제출 일자
+
+2026-08-25 13:27
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
